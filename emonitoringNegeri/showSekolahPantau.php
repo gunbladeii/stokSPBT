fbc6dcd03972e78d4d10f5d43775926e60d670ca
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-03 15:14:40 +0800

    Update showSekolahPantau.php

--- emonitoringNegeri/showSekolahPantau.php
+++ emonitoringNegeri/showSekolahPantau.php
@@ -24,7 +24,6 @@ $year = date('Y');
     $RID2 = mysqli_fetch_assoc($refID3);
 ?>
 <?php
-//if (!empty($RID2['id'])){echo $RID2['sekolahPantau'];}else{echo 0;}
-echo $negeri;
+if (!empty($RID2['id'])){echo $RID2['sekolahPantau'];}else{echo 0;}
 ?>
 

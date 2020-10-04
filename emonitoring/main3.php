53c3d8353d9e17371c3331ca0a8856b32193ddc6
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-04 21:52:16 +0800

    Initial commit

--- emonitoring/main3.php
+++ emonitoring/main3.php
@@ -1,7 +1,9 @@
 <?php require('conn.php'); ?>
 <?php
-if (!isset($_SESSION)) {
-  session_start();
+session_start();
+if ($_SESSION['role'] != 'admin')
+{
+      header('Location:../index.php');
 }
 
 ?>

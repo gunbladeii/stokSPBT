fe6b4c79c0b80d87e639c109b42a89acfef1444e
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-14 18:34:11 +0800

    Update index.php

--- emonitoringNegeri/indexNegeri/index.php
+++ emonitoringNegeri/indexNegeri/index.php
@@ -66,7 +66,7 @@ $a = 1;
 			<div class="wrap-login200 p-t-90 p-b-30">
 				<form class="login200-form validate-form">
 					<span class="login100-form-title p-b-40">
-						<a href="../../index.php"><img src="../img/myspbt_logo.png" style="max-width: 35%; max-height: 25%"></a>
+						<a href="../../logout.php"><img src="../img/myspbt_logo.png" style="max-width: 35%; max-height: 25%"></a>
 					</span>
 
 					<span class="login100-form-title p-b-40">

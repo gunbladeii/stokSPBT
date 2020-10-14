e1cc7e595052c01a75789c9ca2ce4bb781d6ce96
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-14 18:19:57 +0800

    Update index.php

--- emonitoringNegeri/indexNegeri/index.php
+++ emonitoringNegeri/indexNegeri/index.php
@@ -41,7 +41,7 @@ $a = 1;
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
 <!--===============================================================================================-->
@@ -65,7 +65,7 @@ $a = 1;
 		<div class="container-login100">
 			<div class="wrap-login100 p-t-90 p-b-30">
 				<form class="login100-form validate-form">
-					<span class="login100-form-title p-b-100">
+					<span class="login100-form-title p-b-40">
 						Halaman Utama (SPBT Negeri)
 					</span>
 

82fcfedd66e46d89272ca5286235e5072d1b8a87
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-14 18:22:41 +0800

    Update index.php

--- emonitoringNegeri/indexNegeri/index.php
+++ emonitoringNegeri/indexNegeri/index.php
@@ -43,7 +43,7 @@ $a = 1;
 <!--===============================================================================================-->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
 <!--===============================================================================================-->	

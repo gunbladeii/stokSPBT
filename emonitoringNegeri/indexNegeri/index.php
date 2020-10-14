5972105488de96eea4af6238faba034b43b11652
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-14 18:00:30 +0800

    Update index.php

--- emonitoringNegeri/indexNegeri/index.php
+++ emonitoringNegeri/indexNegeri/index.php
@@ -1,3 +1,35 @@
+<?php require('../conn.php'); ?>
+<?php
+session_start();
+if ($_SESSION['role'] != 'stokNegeri')
+{
+      header('Location:../../index.php');
+}
+
+?>
+<?php
+
+date_default_timezone_set("asia/kuala_lumpur");
+$date = date('d-F-Y');
+$datePHP = date('Y-m-d');
+
+$colname_Recordset = "-1";
+if (isset($_SESSION['user'])) {
+  $colname_Recordset = $_SESSION['user'];
+}
+
+
+$Recordset = $mysqli->query("SELECT * FROM login WHERE username = '$colname_Recordset'");
+$row_Recordset = mysqli_fetch_assoc($Recordset);
+$totalRows_Recordset = mysqli_num_rows($Recordset);
+
+$namaSekolah = $_POST['namaSekolah'];
+if (isset($_POST['submit'])) {
+    $mysqli->query ("SELECT * FROM dataSekolah WHERE namaSekolah LIKE '$namaSekolah'");
+    header("location:searching.php?namaSekolah=$namaSekolah");
+    }
+$a = 1;
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -40,50 +72,14 @@
 					<div>
 						<a href="#" class="btn-login-with bg1 m-b-10">
 							<i class="fa fa-facebook-official"></i>
-							Login with Facebook
+							Capaian ke Sistem Pengurusan Stok SPBT
 						</a>
 
 						<a href="#" class="btn-login-with bg2">
 							<i class="fa fa-twitter"></i>
-							Login with Twitter
+							Capaian ke Sistem Pengurusan Perolehan SPBT
 						</a>
 					</div>
-
-					<div class="text-center p-t-55 p-b-30">
-						<span class="txt1">
-							Login with email
-						</span>
-					</div>
-
-					<div class="wrap-input100 validate-input m-b-16" data-validate="Please enter email: ex@abc.xyz">
-						<input class="input100" type="text" name="email" placeholder="Email">
-						<span class="focus-input100"></span>
-					</div>
-
-					<div class="wrap-input100 validate-input m-b-20" data-validate = "Please enter password">
-						<span class="btn-show-pass">
-							<i class="fa fa fa-eye"></i>
-						</span>
-						<input class="input100" type="password" name="pass" placeholder="Password">
-						<span class="focus-input100"></span>
-					</div>
-
-					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
-							Login
-						</button>
-					</div>
-					
-					<div class="flex-col-c p-t-224">
-						<span class="txt2 p-b-10">
-							Don’t have an account?
-						</span>
-
-						<a href="#" class="txt3 bo1 hov1">
-							Sign up now
-						</a>
-					</div>
-					
 				</form>
 			</div>
 		</div>

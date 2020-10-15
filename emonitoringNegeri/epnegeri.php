ad1cc5a2704bbba85f42889d325cb674b345db5f
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-15 22:25:19 +0800

    Update epnegeri.php

--- emonitoringNegeri/epnegeri.php
+++ emonitoringNegeri/epnegeri.php
@@ -397,12 +397,12 @@ $a = 1;
                                 </tr>
 
                                 <tr>
-                                  <?do {?>
+                                  <?php do {?>
                                   <td><?php echo $a++;?></td>
                                   <td><a data-toggle="modal" data-target="#updateDataSHModal" data-whatever="<?php echo $dataSH['id'];?>" class="btn btn-info btn-sm active" role="button" aria-pressed="true"><?php echo strtoupper($dataSH['namaPembekal']);?></a></td>
                                   <td><?php $date=date_create($dataSH['tarikhSHSBegin']);echo date_format($date,"d-m-Y");?></td>
                                   <td><?php echo number_format($dataSH['nilaiSH']);?></td>
-                                  <?} while ($dataSH = mysqli_fetch_assoc($Recordset2));?>
+                                  <?php } while ($dataSH = mysqli_fetch_assoc($Recordset2));?>
                                 </tr>
 
                               </tbody>

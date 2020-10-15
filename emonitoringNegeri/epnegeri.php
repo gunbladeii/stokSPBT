d1ef9d26e315737fda8fdff5559400615094ab74
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-15 11:57:09 +0800

    Update epnegeri.php

--- emonitoringNegeri/epnegeri.php
+++ emonitoringNegeri/epnegeri.php
@@ -318,7 +318,7 @@ $a = 1;
               </div>
               <!-- /.card-header -->
               <div class="card-body p-0">
-                        <?php if (!empty($dataSSH)){?>
+                        <?php if (!empty($dataSH)){?>
                         <div class="table-responsive">
                           <form method="post" action="epnegeri.php" role="form" enctype="multipart/form-data">
                             <table id="example1" class="table m-0">

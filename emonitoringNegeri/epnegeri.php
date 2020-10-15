abf3b41ccb00035e7916eb1fb2f7d12fec1e5ce6
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-15 14:53:05 +0800

    Update epnegeri.php

--- emonitoringNegeri/epnegeri.php
+++ emonitoringNegeri/epnegeri.php
@@ -398,7 +398,7 @@ $a = 1;
                                 <tr>
                                   <td><?php echo $a++;?></td>
                                   <td><a data-toggle="modal" data-target="#updateDataSHModal" data-whatever="<?php echo $dataSH['namaPembekal'];?>" class="btn btn-info btn-sm active" role="button" aria-pressed="true"><?php echo strtoupper($dataSH['namaPembekal']);?></a></td>
-                                  <td><?php echo $dataSH['tarikhSHSBegin'];?></td>
+                                  <td><?php $date=date_create($dataSH['tarikhSHSBegin']);echo date_format($date,"d-m-Y");?></td>
                                 </tr>
 
                               </tbody>

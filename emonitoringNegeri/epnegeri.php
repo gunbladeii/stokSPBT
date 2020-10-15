be100dc708ebff1755a3eafcfd355281e5a7a7bd
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-15 10:33:25 +0800

    Initial commit

--- emonitoringNegeri/epnegeri.php
+++ emonitoringNegeri/epnegeri.php
@@ -103,7 +103,7 @@ $a = 1;
         <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="main1.php" class="nav-link">Home</a>
+        <a href="epnegeri.php" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
@@ -211,7 +211,7 @@ $a = 1;
       </li>
       <!-- Exit -->
       <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="main1.php">
+        <a class="nav-link" data-toggle="dropdown" href="epnegeri.php">
           <i class="far fa-times-circle"></i>
         </a>
         <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
@@ -227,7 +227,7 @@ $a = 1;
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-light-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="main1.php" class="brand-link">
+    <a href="epnegeri.php" class="brand-link">
       <img src="../adminSPBT/dist/img/logo_kpm.png" alt="altus Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-dark" style="font-family: 'Fugaz One', cursive;">mySPBT 2.0</span>
@@ -248,7 +248,7 @@ $a = 1;
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item has-treeview menu-open">
-            <a href="main1.php" class="nav-link active">
+            <a href="epnegeri.php" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                  mySPBT 2.0 Dashboard
@@ -290,7 +290,7 @@ $a = 1;
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="main1.php">Home</a></li>
+              <li class="breadcrumb-item"><a href="epnegeri.php">Home</a></li>
               <li class="breadcrumb-item active">mySPBT 2.0 | Pengesanan SH-Negeri</li>
             </ol>
           </div><!-- /.col -->
@@ -320,7 +320,7 @@ $a = 1;
               <div class="card-body p-0">
                         <?php if (!empty($dataSSH['username'])){?>
                         <div class="table-responsive">
-                          <form method="post" action="epnegeri.php" role="form" enctype="multipart/form-data">
+                          <form method="post" action="epnegeri2.php" role="form" enctype="multipart/form-data">
                             <table id="example1" class="table m-0">
                               <thead>
                                 <tr>
@@ -568,7 +568,7 @@ $a = 1;
 
                           <?php {?>
                           <div class="table-responsive">
-                          <form method="post" action="epnegeri.php" role="form" enctype="multipart/form-data">
+                          <form method="post" action="epnegeri2.php" role="form" enctype="multipart/form-data">
                             <table id="example1" class="table m-0">
                               <thead>
                                 <tr>
--- emonitoringNegeri/epnegeri2.php
+++ emonitoringNegeri/epnegeri2.php
@@ -73,7 +73,7 @@ $a = 1;
         <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="main1.php" class="nav-link">Home</a>
+        <a href="epnegeri.php" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
@@ -181,7 +181,7 @@ $a = 1;
       </li>
       <!-- Exit -->
       <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="main1.php">
+        <a class="nav-link" data-toggle="dropdown" href="epnegeri.php">
           <i class="far fa-times-circle"></i>
         </a>
         <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
@@ -197,7 +197,7 @@ $a = 1;
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-light-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="main1.php" class="brand-link">
+    <a href="epnegeri.php" class="brand-link">
       <img src="../adminSPBT/dist/img/logo_kpm.png" alt="altus Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-dark" style="font-family: 'Fugaz One', cursive;">mySPBT 2.0</span>
@@ -218,7 +218,7 @@ $a = 1;
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item has-treeview menu-open">
-            <a href="main1.php" class="nav-link active">
+            <a href="epnegeri.php" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                  mySPBT 2.0 Dashboard
@@ -260,7 +260,7 @@ $a = 1;
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="main1.php">Home</a></li>
+              <li class="breadcrumb-item"><a href="epnegeri.php">Home</a></li>
               <li class="breadcrumb-item active">mySPBT 2.0 | Pengesanan SH-Negeri</li>
             </ol>
           </div><!-- /.col -->

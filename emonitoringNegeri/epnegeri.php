b86224d51175f99990ae6dfb6b953f02fcf3ccd7
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-15 11:54:16 +0800

    Update epnegeri.php

--- emonitoringNegeri/epnegeri.php
+++ emonitoringNegeri/epnegeri.php
@@ -566,7 +566,7 @@ $a = 1;
                           </div>
                           <?php }?>
 
-                          <?php if($dataSH['username'] == NULL){?>
+                          <?php if(empty($dataSH['username'])){?>
                           <div class="table-responsive">
                           <form method="post" action="epnegeri.php" role="form" enctype="multipart/form-data">
                             <table id="example1" class="table m-0">

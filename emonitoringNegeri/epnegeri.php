bd092479fdac0f2177a9ea8b5d7da1cdf45f662f
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-15 14:57:55 +0800

    Update epnegeri.php

--- emonitoringNegeri/epnegeri.php
+++ emonitoringNegeri/epnegeri.php
@@ -393,12 +393,14 @@ $a = 1;
                                   <th>Bil</th>
                                   <th>Nama Pembekal</th>
                                   <th>Tarikh Mula Sebut Harga</th>
+                                  <th>Nilai Sebut Harga</th>
                                 </tr>
 
                                 <tr>
                                   <td><?php echo $a++;?></td>
                                   <td><a data-toggle="modal" data-target="#updateDataSHModal" data-whatever="<?php echo $dataSH['namaPembekal'];?>" class="btn btn-info btn-sm active" role="button" aria-pressed="true"><?php echo strtoupper($dataSH['namaPembekal']);?></a></td>
                                   <td><?php $date=date_create($dataSH['tarikhSHSBegin']);echo date_format($date,"d-m-Y");?></td>
+                                  <td><?php setlocale(LC_MONETARY,"en_MYR");echo money_format("%i", $dataSH['nilaiSH']);?></td>
                                 </tr>
 
                               </tbody>

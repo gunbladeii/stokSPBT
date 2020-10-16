cfd2b8bec36a00f1329be00a0539bb59b9acc166
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-16 12:31:08 +0800

    Update chartSH.js

--- emonitoring/chartSH.js
+++ emonitoring/chartSH.js
@@ -4,23 +4,23 @@ google.charts.setOnLoadCallback(drawChart);
     function drawChart() {
         // Some raw data (not necessarily accurate)
         var data = google.visualization.arrayToDataTable([
-          ['Negeri', { role: 'annotation' },'Nilai(RM)', { role: 'style' }],
-          ['Sabah', 'RM',  21655, '#b87333'],
-          ['Sarawak', 'RM',  134500, 'silver'],
-          ['WP Labuan', 'RM',  215000, 'gold'],
-          ['WP Kuala Lumpur', 'RM',  139000, '#65eb34'],
-          ['Kedah', 'RM',  14000, '#eb34dc'],
-          ['Perlis', 'RM',  219000, '#eb3493'],
-          ['Pulau Pinang', 'RM',  34000, '#8034eb'],
-          ['Melaka', 'RM',  13901, '#5c34eb'],
-          ['Selangor', 'RM',  145000, '#5c34eb'],
-          ['Perak', 'RM',  150000, '#6c6096'],
-          ['Negeri Sembilan', 'RM',  230000, '#609196'],
-          ['Pahang', 'RM',  254000, '#609692'],
-          ['Kelantan',  415000, 'RM', '#609689'],
-          ['Terengganu', 'RM',  23000, '#609676'],
-          ['Johor', 'RM',  111000, '#609662'],
-          ['WP Putrajaya', 'RM',  13126, '#ae34eb']
+          ['Negeri', 'Nilai(RM)', { role: 'style' }],
+          ['Sabah',  21655, '#b87333'],
+          ['Sarawak',  134500, 'silver'],
+          ['WP Labuan',  215000, 'gold'],
+          ['WP Kuala Lumpur',  139000, '#65eb34'],
+          ['Kedah',  14000, '#eb34dc'],
+          ['Perlis',  219000, '#eb3493'],
+          ['Pulau Pinang',  34000, '#8034eb'],
+          ['Melaka',  13901, '#5c34eb'],
+          ['Selangor',  145000, '#5c34eb'],
+          ['Perak',  150000, '#6c6096'],
+          ['Negeri Sembilan',  230000, '#609196'],
+          ['Pahang',  254000, '#609692'],
+          ['Kelantan',  415000, '#609689'],
+          ['Terengganu',  23000, '#609676'],
+          ['Johor',  111000, '#609662'],
+          ['WP Putrajaya',  13126, '#ae34eb']
         ]);
 
         var view = new google.visualization.DataView(data);

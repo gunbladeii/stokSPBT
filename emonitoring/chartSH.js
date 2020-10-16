570ec2ffcb7e64303c4734b6794cb49825873188
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-16 11:27:25 +0800

    Update chartSH.js

--- emonitoring/chartSH.js
+++ emonitoring/chartSH.js
@@ -4,12 +4,12 @@ google.charts.setOnLoadCallback(drawVisualization);
       function drawVisualization() {
         // Some raw data (not necessarily accurate)
         var data = google.visualization.arrayToDataTable([
-          
-          ['Sabah',  165,      938,         522,             998,           450,      614.6],
-          ['Sarawak',  135,      1120,        599,             1268,          288,      682],
-          ['WP Labuan',  157,      1167,        587,             807,           397,      623],
-          ['WP Kuala Lumpur',  139,      1110,        615,             968,           215,      609.4],
-          ['WP Putrajaya',  136,      691,         629,             1026,          366,      569.6]
+          ['Negeri', 'Nilai(RM)'],
+          ['Sabah',  165],
+          ['Sarawak',  135],
+          ['WP Labuan',  157],
+          ['WP Kuala Lumpur',  139],
+          ['WP Putrajaya',  136]
         ]);
 
         var options = {

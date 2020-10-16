5eb0101ad58608e23c91d41bd4d68b7ee4148b06
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-16 11:25:01 +0800

    Update chartSH.js

--- emonitoring/chartSH.js
+++ emonitoring/chartSH.js
@@ -1,21 +1,21 @@
 google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawVisualization);
+google.charts.setOnLoadCallback(drawVisualization);
 
       function drawVisualization() {
         // Some raw data (not necessarily accurate)
         var data = google.visualization.arrayToDataTable([
-          ['Month', 'Bolivia', 'Ecuador', 'Madagascar', 'Papua New Guinea', 'Rwanda', 'Average'],
-          ['2004/05',  165,      938,         522,             998,           450,      614.6],
-          ['2005/06',  135,      1120,        599,             1268,          288,      682],
-          ['2006/07',  157,      1167,        587,             807,           397,      623],
-          ['2007/08',  139,      1110,        615,             968,           215,      609.4],
-          ['2008/09',  136,      691,         629,             1026,          366,      569.6]
+          
+          ['Sabah',  165,      938,         522,             998,           450,      614.6],
+          ['Sarawak',  135,      1120,        599,             1268,          288,      682],
+          ['WP Labuan',  157,      1167,        587,             807,           397,      623],
+          ['WP Kuala Lumpur',  139,      1110,        615,             968,           215,      609.4],
+          ['WP Putrajaya',  136,      691,         629,             1026,          366,      569.6]
         ]);
 
         var options = {
-          title : 'Monthly Coffee Production by Country',
-          vAxis: {title: 'Cups'},
-          hAxis: {title: 'Month'},
+          title : 'Perolehan Sebut Harga Setiap Negeri',
+          vAxis: {title: 'Nilai(RM)'},
+          hAxis: {title: 'Negeri'},
           seriesType: 'bars',
           series: {5: {type: 'line'}}
         };

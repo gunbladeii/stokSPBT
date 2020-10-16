a08057370383ad97ca944619b30020f5256a316d
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-16 15:34:59 +0800

    Update chartSH.php

--- emonitoring/chartSH.php
+++ emonitoring/chartSH.php
@@ -69,6 +69,6 @@ google.charts.setOnLoadCallback(drawChart);
       }
 </script>
 
-<?php if(!empty($dataSH2)) { ?>
+<?php if(!empty($dataSH2['negeri'])) { ?>
 <div id="chart_div" style="width: 100%; height: 100%;"></div>
 <?php } else echo '<a class="btn btn-warning btn-sm active" role="button" aria-pressed="true">Tiada rekod setakat ini</a>';?>

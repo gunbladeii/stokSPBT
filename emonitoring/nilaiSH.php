e3a7df0809b6360d8f1f830313a4f5d750df9b12
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-16 17:49:28 +0800

    Update nilaiSH.php

--- emonitoring/nilaiSH.php
+++ emonitoring/nilaiSH.php
@@ -70,5 +70,5 @@ google.charts.setOnLoadCallback(drawChart);
 
 <?php if(!empty($dataSH2['negeri'])) { ?>
 <h3 style="font-family: 'Roboto Condensed', sans-serif; text-align: center;">Nilai Perolehan Sebut Harga setiap negeri</h3>
-<div class="img-fluid" id="nilai_div" style="width: 65%;"></div>
+<div class="table-responsive" id="nilai_div" style="width: 65%;"></div>
 <?php ;} else echo '<a class="btn btn-warning btn-sm active" role="button" aria-pressed="true">Tiada rekod setakat ini</a>';?>

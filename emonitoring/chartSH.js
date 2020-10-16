e5750db2ce55bc84a6b24fd46f2780f50fdafa22
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-16 11:38:00 +0800

    Update chartSH.js

--- emonitoring/chartSH.js
+++ emonitoring/chartSH.js
@@ -5,11 +5,22 @@ google.charts.setOnLoadCallback(drawVisualization);
         // Some raw data (not necessarily accurate)
         var data = google.visualization.arrayToDataTable([
           ['Negeri', 'Nilai(RM)', { role: 'style' }],
-          ['Sabah',  165, '#b87333'],
-          ['Sarawak',  135, 'silver'],
-          ['WP Labuan',  157, 'gold'],
-          ['WP Kuala Lumpur',  139, '#65eb34'],
-          ['WP Putrajaya',  136, '#ae34eb']
+          ['Sabah',  21655, '#b87333'],
+          ['Sarawak',  134500, 'silver'],
+          ['WP Labuan',  215000, 'gold'],
+          ['WP Kuala Lumpur',  139000, '#65eb34'],
+          ['Kedah',  14000, '#eb34dc'],
+          ['Perlis',  219000, '#eb3493'],
+          ['Pulau Pinang',  34000, '#8034eb'],
+          ['Melaka',  13901, '#5c34eb'],
+          ['Selangor',  145000, '#5c34eb'],
+          ['Perak',  150000, '#6c6096'],
+          ['Negeri Sembilan',  230000, '#609196'],
+          ['Pahang',  254000, '#609692'],
+          ['Kelantan',  415000, '#609689'],
+          ['Terengganu',  23000, '#609676'],
+          ['Johor',  111000, '#609662'],
+          ['WP Putrajaya',  13126, '#ae34eb']
         ]);
 
         var options = {

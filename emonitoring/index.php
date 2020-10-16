11afe97711846e41e6be9d8becd47cb85422c5c9
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-16 10:28:47 +0800

    Update index.php

--- emonitoring/index.php
+++ emonitoring/index.php
@@ -274,7 +274,7 @@ $a = 1;
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-cogs"></i>
+              <!--<i class="nav-icon fas fa-cogs"></i>-->
               <p>
                 
               </p>

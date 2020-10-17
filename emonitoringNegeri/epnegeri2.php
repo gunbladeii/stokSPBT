9b2d3b39507269591e0836d9dde458d14112d14a
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-17 17:25:52 +0800

    Update epnegeri2.php

--- emonitoringNegeri/epnegeri2.php
+++ emonitoringNegeri/epnegeri2.php
@@ -272,7 +272,7 @@ $a = 1;
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-light-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="epenilai2.php" class="brand-link">
+    <a href="epnegeri2.php" class="brand-link">
       <img src="../adminSPBT/dist/img/logo_kpm.png" alt="altus Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-dark" style="font-family: 'Fugaz One', cursive;">mySPBT 2.0</span>

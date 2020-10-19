8a4941d6a30cde40b127441415471a5549ca4e3c
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-19 23:55:43 +0800

    Update fetchdataJudulSH.php

--- emonitoringNegeri/fetchdataJudulSH.php
+++ emonitoringNegeri/fetchdataJudulSH.php
@@ -4,12 +4,13 @@ $connect = mysqli_connect("localhost", "adminspbt", "Sh@ti5620", "spbt_stok");
 $output = '';
 $query = "SELECT dataSHJudulPenerbit.kodJudul, dataSHJudul.judul FROM dataSHJudulPenerbit INNER JOIN dataSHJudul ON dataSHJudulPenerbit.kodJudul = dataSHJudul.kodJudul ORDER BY dataSHJudulPenerbit.kodJudul ASC";
 $result = mysqli_query($connect, $query);
+$a = 1;
 $output = '
 <br />
 <h3 align="center">Judul</h3>
 <table class="table table-bordered table-striped">
  <tr>
-  <th width="5%">id</th>
+  <th width="5%">No</th>
   <th width="30%">Kod Judul</th>
   <th width="65%">Nama Judul</th>
  </tr>
@@ -18,9 +19,9 @@ while($row = mysqli_fetch_array($result))
 {
  $output .= '
  <tr>
-  <td>'.$row["id"].'</td>
+  <td>'.$a++.'</td>
   <td>'.$row["kodJudul"].'</td>
-  <td>'.$row["namaJudul"].'</td>
+  <td>'.$row["judul"].'</td>
  </tr>
  ';
 }

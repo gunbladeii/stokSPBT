610564c7f7a922c184b5f216c12fbf8bdff2163f
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-19 21:59:48 +0800

    Update daftarJudulSHModal.php

--- emonitoringNegeri/daftarJudulSHModal.php
+++ emonitoringNegeri/daftarJudulSHModal.php
@@ -36,7 +36,10 @@
     $dataSH = mysqli_fetch_assoc($Recordset2);
     $totalRows_Recordset2 = mysqli_num_rows($Recordset2);
 
-?>                   
+?>    
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>               
                       
                           <div class="container">
                           <form method="post" id="insert_form">

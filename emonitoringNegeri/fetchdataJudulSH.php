4e30925a9fd56ae8f600968817e06eaa250efb86
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-20 16:26:34 +0800

    Update fetchdataJudulSH.php

--- emonitoringNegeri/fetchdataJudulSH.php
+++ emonitoringNegeri/fetchdataJudulSH.php
@@ -20,24 +20,24 @@ if (mysqli_num_rows($result) > 0)
 		<div class="table-responsive">
 		<table class="table table-bordered table-sm">
 		 <tr class="bg-warning">
-		  <th><button type="button" name="delete_all" id="delete_all" class="btn btn-danger btn-xs">Padam</button></th>
 		  <th width="5%">No</th>
 		  <th width="20%">Kod Judul</th>
 		  <th width="50%">Nama Judul</th>
 		  <th width="10%">Pembekal</th>
 		  <th width="10%">Negeri</th>
+		  <th><button type="button" name="delete_all" id="delete_all" class="btn btn-danger btn-xs">Padam</button></th>
 		 </tr>
 		';
 		while($row = mysqli_fetch_array($result))
 		{
 		 $output .= '
 		 <tr>
-		  <td><input type="checkbox" class="delete_checkbox" value="'.$row["id"].'" /></td>
 		  <td>'.$a++.'</td>
 		  <td>'.$row["kodJudul"].'</td>
 		  <td>'.$row["judul"].'</td>
 		  <td>'.strtoupper($row["namaPembekal"]).'</td>
 		  <td>'.$row["negeri"].'</td>
+		  <td><input type="checkbox" class="delete_checkbox" value="'.$row["id"].'" /></td>
 		 </tr>
 		 ';
 		}

4d2dc189f15296f065d963be722d655c59962a67
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-21 18:15:39 +0800

    Update deleteJudulSH.php

--- emonitoringNegeri/deleteJudulSH.php
+++ emonitoringNegeri/deleteJudulSH.php
@@ -8,7 +8,7 @@ if(isset($_POST["checkbox_value"]))
 {
  for($count = 0; $count < count($_POST["checkbox_value"]); $count++)
  {
-  $query = "DELETE FROM dataSHJudulPenerbit WHERE id = '".$_POST['checkbox_value'][$count]."'";
+  $query = "DELETE FROM dataJudulPenerbit WHERE id = '".$_POST['checkbox_value'][$count]."'";
   $statement = $connect->prepare($query);
   $statement->execute();
  }

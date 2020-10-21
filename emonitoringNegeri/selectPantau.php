271d17e8ceb5383e5eba9a1f23907f5515b8b68f
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-22 02:09:20 +0800

    Update selectPantau.php

--- emonitoringNegeri/selectPantau.php
+++ emonitoringNegeri/selectPantau.php
@@ -12,11 +12,7 @@ $connect = new PDO("mysql:host=localhost;dbname=spbt_stok", "adminspbt", "Sh@ti5
 $namaPembekal = $_GET['namaPembekal'];
 $negeri = $_GET['negeri'];
 
-$query = "SELECT dataJudulPenerbit.id,dataJudulPenerbit.timestamp,dataJudulPenerbit.namapembekal, dataJudulPenerbit.negeri,dataJudulPenerbit.kodjudul,dataJudulPenerbit.bilnaskhahpesan, dataJudulPenerbit.bilnaskhahbekal, dataJudulPenerbit.statusbekal,dataSHJudul.judul FROM 
-(dataJudulPenerbit 
-	INNER JOIN dataSHJudul ON dataJudulPenerbit.kodjudul = dataSHJudul.kodJudul)
-	WHERE dataJudulPenerbit.namapembekal = '$namaPembekal' AND dataJudulPenerbit.negeri = '$negeri'
-	  ORDER BY dataJudulPenerbit.id DESC
+$query = "SELECT * FROM dataJudulPenerbit WHERE namapembekal = '$namaPembekal' AND negeri = '$negeri' ORDER BY id DESC";
 
 $statement = $connect->prepare($query);
 

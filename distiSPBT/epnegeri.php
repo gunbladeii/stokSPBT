cd9f69e4de548955c55155413c8f3aedd29fbfaa
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-23 10:21:12 +0800

    Update epnegeri.php

--- distiSPBT/epnegeri.php
+++ distiSPBT/epnegeri.php
@@ -274,7 +274,7 @@ $a = 1;
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="info">
-          <a href="#" class="d-block"><?php echo strtoupper($row_Recordset['nama']);?></a>
+          
         </div>
       </div>
 

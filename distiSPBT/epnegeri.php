d57018bb719da16b064a2f8aaf19cdb5ec403a41
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-23 17:40:39 +0800

    Update epnegeri.php

--- distiSPBT/epnegeri.php
+++ distiSPBT/epnegeri.php
@@ -319,7 +319,7 @@ $a = 1;
                                   <th>Bil. Judul Pembekalan</th>
                                   <?php }?>
                                   <th>Pengesanan Pembekalan</th>
-                                  <th>Hapus Judul</th>
+                                  <th><i class="badge badge-danger fas fa-times-circle"></i></th>
                                   <th><button type="button" name="delete_all" id="delete_all" class="btn btn-danger btn-xs">Hapus</button></th>
                                 </tr>
 

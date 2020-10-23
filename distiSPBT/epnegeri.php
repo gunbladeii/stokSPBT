b52182f7235d5d468d326aef09e1c251a3a7a6f3
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-23 10:44:34 +0800

    Update epnegeri.php

--- distiSPBT/epnegeri.php
+++ distiSPBT/epnegeri.php
@@ -23,7 +23,7 @@ $Recordset = $mysqli->query("SELECT * FROM login WHERE username = '$colname_Reco
 $row_Recordset = mysqli_fetch_assoc($Recordset);
 $totalRows_Recordset = mysqli_num_rows($Recordset);
 
-$Recordset2 = $mysqli->query("SELECT * FROM dataSH WHERE username = '$colname_Recordset'");
+$Recordset2 = $mysqli->query("SELECT * FROM dataSH WHERE kodPembekal = '$colname_Recordset'");
 $dataSH = mysqli_fetch_assoc($Recordset2);
 $totalRows_Recordset2 = mysqli_num_rows($Recordset2);
 

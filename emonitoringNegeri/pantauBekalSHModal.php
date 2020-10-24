ed4a5418f3b78ae5ac523ba8d357fad42edc4522
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-24 17:05:02 +0800

    Update pantauBekalSHModal.php

--- emonitoringNegeri/pantauBekalSHModal.php
+++ emonitoringNegeri/pantauBekalSHModal.php
@@ -398,7 +398,7 @@ $(document).ready(function(){
                     html += '<td>'+data[count].judul+'</td>';
                     html += '<td>'+data[count].bilnaskhahpesan+'</td>';
                     html += '<td>'+data[count].bilnaskhahbekal+'</td>';
-                    html += '<td>'+data[count].statusbekal+'</td></tr>';
+                    html += '<td style="color:red">'+data[count].statusbekal+'</td></tr>';
                 }
                 $('tbody').html(html);
             }
@@ -425,7 +425,7 @@ $(document).ready(function(){
             html += '<td>'+$(this).data('judul')+'</td>';
             html += '<td>'+$(this).data('bilnaskhahpesan')+'</td>';
             html += '<td>'+$(this).data('bilnaskhahbekal')+'</td>';
-            html += '<td>'+$(this).data('statusbekal')+'</td>';            
+            html += '<td style="color:red>'+$(this).data('statusbekal')+'</td>';            
         }
         $(this).closest('tr').html(html);
         $('#statusbekal_'+$(this).attr('id')+'').val($(this).data('statusbekal'));

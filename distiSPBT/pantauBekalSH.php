420dd455b5fa30100dabc909573364e295d6e9d2
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-24 21:43:55 +0800

    Update pantauBekalSH.php

--- distiSPBT/pantauBekalSH.php
+++ distiSPBT/pantauBekalSH.php
@@ -218,27 +218,7 @@ $a = 1;
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item has-treeview menu-open">
-            <a href="epnegeri.php" class="nav-link active">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                 mySPBT 2.0 Dashboard
-                <!--<i class="right fas fa-angle-left"></i>-->
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-             
-             
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-cogs"></i>
-              <p>
-                Dashboard
-              </p>
-            </a>
-          </li>
+         
           
         </ul>
       </nav>

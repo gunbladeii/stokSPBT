c2c892b528bf0f94c135b5c680225219628e13d4
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-25 20:55:44 +0800

    Update index.php

--- emonitoringNegeri/indexNegeri/index.php
+++ emonitoringNegeri/indexNegeri/index.php
@@ -59,7 +59,52 @@ $a = 1;
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 <!--===============================================================================================-->
 </head>
-<body>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+	<!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light border-bottom">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="indexNegeri/index.php" class="nav-link">mySPBT</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="#" class="nav-link"></a>
+      </li>
+    </ul>
+
+    <!-- SEARCH FORM -->
+    <form class="form-inline ml-3">
+      <div class="input-group input-group-sm">
+        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-navbar" type="submit">
+            <i class="fas fa-search"></i>
+          </button>
+        </div>
+      </div>
+    </form>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      <!-- Exit -->
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="epnegeri.php">
+          <i class="far fa-times-circle"></i>
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <div class="dropdown-divider"></div>
+          <a href="../../logout.php" class="dropdown-item dropdown-footer">Logout</a>
+        </div>
+      </li>
+     
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+
 	
 	<div class="limiter">
 		<div class="container-login100">
@@ -92,7 +137,7 @@ $a = 1;
 			</div>
 		</div>
 	</div>
-	
+</div>	
 	
 <!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>

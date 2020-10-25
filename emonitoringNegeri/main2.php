a4aee4e30411e16f976290c1f897735a36af8768
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-25 20:50:04 +0800

    Initial commit

--- emonitoringNegeri/main2.php
+++ emonitoringNegeri/main2.php
@@ -92,7 +92,7 @@ $a = 1;
         <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="indexNegeri/index.php" class="nav-link">Halaman utama</a>
+        <a href="indexNegeri/index.php" class="nav-link">mySPBT</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link"></a>

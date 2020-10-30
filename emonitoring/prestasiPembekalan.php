f629bdf6eb126a087e8a01ae7f1d3fb9c5b0db03
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-10-30 12:47:32 +0800

    Update prestasiPembekalan.php

--- emonitoring/prestasiPembekalan.php
+++ emonitoring/prestasiPembekalan.php
@@ -54,9 +54,9 @@ google.charts.setOnLoadCallback(drawChart);
     function drawChart() {
         // Some raw data (not necessarily accurate)
         var data = google.visualization.arrayToDataTable([
-          ['Negeri', 'Bilangan Naskhah Pesan','Bilangan Naskhah Bekal', 'Peratus Bekal(%)'],
+          ['Negeri', 'Bilangan Naskhah Pesan','Bilangan Naskhah Bekal'],
           <?php do { ?>
-          ['<?php echo $dataJudulPenerbit["negeri"];?>',  <?php echo $dataJudulPenerbit["bilnaskhahpesan"];?>, <?php echo $dataJudulPenerbit["bilnaskhahbekal"];?>, <?php echo $dataJudulPenerbit["peratusbekal"];?>],
+          ['<?php echo $dataJudulPenerbit["negeri"];?>',  <?php echo $dataJudulPenerbit["bilnaskhahpesan"];?>, <?php echo $dataJudulPenerbit["bilnaskhahbekal"];?>],
           <?php } while ($dataJudulPenerbit = mysqli_fetch_assoc($Recordset2));?>
         ]);
 

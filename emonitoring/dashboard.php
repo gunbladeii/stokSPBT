8f962a5ddc73dcc5c65226df366f72947b37127f
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-12-24 07:34:39 +0800

    Update dashboard.php

--- emonitoring/dashboard.php
+++ emonitoring/dashboard.php
@@ -34,12 +34,8 @@ $downloadExcell = $_SERVER['PHP_SELF'];
 /*advanced*/
 if (isset($_POST["stok"]))
   {
-  $sql = $mysqli->query("SELECT dataSekolah.tarikhPemantauan,dataSekolah.daerah,dataSekolah.noTelefon,dataSekolah.kodSekolah,dataSekolah.namaSekolah,dataSekolah.kategori,dataSekolah.negeri, SUM(rekodPemantauan.bukuLebihan) AS bukuLebihan, 
-  SUM(
-  CASE 
-  WHEN dataSekolah.kategori = 'BOSS' AND rekodPemantauan.bukuStok > 0  THEN rekodPemantauan.bukuStok
-  WHEN dataSekolah.kategori = 'BOSD' THEN 0   
-  ELSE 0 END) AS bukuStok 
+  $sql = $mysqli->query("SELECT DATE_FORMAT(dataSekolah.tarikhPemantauan, '%d-%m-%y') as tarikhPemantauan,dataSekolah.daerah,dataSekolah.noTelefon,dataSekolah.kodSekolah,dataSekolah.namaSekolah,dataSekolah.kategori,dataSekolah.negeri, SUM(rekodPemantauan.bukuLebihan) AS bukuLebihan
+  
   FROM ((rekodPemantauan 
   INNER JOIN dataJudul ON rekodPemantauan.kodJudul = dataJudul.kodJudul)
   INNER JOIN dataSekolah ON rekodPemantauan.kodSekolah = dataSekolah.kodSekolah)

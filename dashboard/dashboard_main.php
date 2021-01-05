0b0a94a620e6ee43c52985e47082ec2edcf152bc
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2021-01-05 12:39:37 +0800

    Initial commit

--- dashboard/dashboard_main.php
+++ dashboard/dashboard_main.php
@@ -137,20 +137,13 @@ $b = 1;
   <link rel="stylesheet" href="../adminSPBT/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Tempusdominus Bbootstrap 4 -->
-  <link rel="stylesheet" href="../adminSPBT/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="../adminSPBT/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- JQVMap -->
-  <link rel="stylesheet" href="../adminSPBT/plugins/jqvmap/jqvmap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="../adminSPBT/dist/css/adminlte.min.css">
-  <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="../adminSPBT/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="../adminSPBT/plugins/daterangepicker/daterangepicker.css">
-  <!-- summernote -->
-  <link rel="stylesheet" href="../adminSPBT/plugins/summernote/summernote-bs4.css">
+  <link href="../lib/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="../lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="../lib/typicons.font/typicons.css" rel="stylesheet">
+  <link href="../lib/flag-icon-css/css/flag-icon.min.css" rel="stylesheet">
+
+  <!-- azia CSS -->
+  <link rel="stylesheet" href="../css/azia.css">
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="styleSearchJudul.css">
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">

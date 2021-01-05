037239bd2ae8752a1d15d92f5cc743b6233f7e90
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2021-01-05 09:34:52 +0800

    Update dashboard_main.php

--- dashboard_main.php
+++ dashboard_main.php
@@ -134,23 +134,23 @@ $b = 1;
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="../adminSPBT/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="adminSPBT/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bbootstrap 4 -->
-  <link rel="stylesheet" href="../adminSPBT/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <link rel="stylesheet" href="adminSPBT/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="../adminSPBT/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="adminSPBT/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- JQVMap -->
-  <link rel="stylesheet" href="../adminSPBT/plugins/jqvmap/jqvmap.min.css">
+  <link rel="stylesheet" href="adminSPBT/plugins/jqvmap/jqvmap.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="../adminSPBT/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="adminSPBT/dist/css/adminlte.min.css">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="../adminSPBT/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="adminSPBT/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="../adminSPBT/plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="adminSPBT/plugins/daterangepicker/daterangepicker.css">
   <!-- summernote -->
-  <link rel="stylesheet" href="../adminSPBT/plugins/summernote/summernote-bs4.css">
+  <link rel="stylesheet" href="adminSPBT/plugins/summernote/summernote-bs4.css">
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="styleSearchJudul.css">
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
@@ -211,7 +211,7 @@ $b = 1;
   <aside class="main-sidebar sidebar-light-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index.php" class="brand-link">
-      <img src="../adminSPBT/dist/img/logo_kpm.png" alt="altus Logo" class="brand-image img-circle elevation-3"
+      <img src="adminSPBT/dist/img/logo_kpm.png" alt="altus Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-dark" style="font-family: 'Fugaz One', cursive;">mySPBT 2.0</span>
     </a>
@@ -421,41 +421,41 @@ $b = 1;
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="../adminSPBT/plugins/jquery/jquery.min.js"></script>
+<script src="adminSPBT/plugins/jquery/jquery.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="../adminSPBT/plugins/jquery-ui/jquery-ui.min.js"></script>
+<script src="adminSPBT/plugins/jquery-ui/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="../adminSPBT/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="adminSPBT/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- ChartJS -->
-<script src="../adminSPBT/plugins/chart.js/Chart.min.js"></script>
+<script src="adminSPBT/plugins/chart.js/Chart.min.js"></script>
 <!-- Sparkline -->
-<script src="../adminSPBT/plugins/sparklines/sparkline.js"></script>
+<script src="adminSPBT/plugins/sparklines/sparkline.js"></script>
 <!-- JQVMap -->
-<script src="../adminSPBT/plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="../adminSPBT/plugins/jqvmap/maps/jquery.vmap.world.js"></script>
+<script src="adminSPBT/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="adminSPBT/plugins/jqvmap/maps/jquery.vmap.world.js"></script>
 <!-- jQuery Knob Chart -->
-<script src="../adminSPBT/plugins/jquery-knob/jquery.knob.min.js"></script>
+<script src="adminSPBT/plugins/jquery-knob/jquery.knob.min.js"></script>
 <!-- daterangepicker -->
-<script src="../adminSPBT/plugins/moment/moment.min.js"></script>
-<script src="../adminSPBT/plugins/daterangepicker/daterangepicker.js"></script>
+<script src="adminSPBT/plugins/moment/moment.min.js"></script>
+<script src="adminSPBT/plugins/daterangepicker/daterangepicker.js"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="../adminSPBT/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="adminSPBT/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
 <!-- Summernote -->
-<script src="../adminSPBT/plugins/summernote/summernote-bs4.min.js"></script>
+<script src="adminSPBT/plugins/summernote/summernote-bs4.min.js"></script>
 <!-- overlayScrollbars -->
-<script src="../adminSPBT/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="adminSPBT/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- FastClick -->
-<script src="../adminSPBT/plugins/fastclick/fastclick.js"></script>
+<script src="adminSPBT/plugins/fastclick/fastclick.js"></script>
 <!-- AdminLTE App -->
-<script src="../adminSPBT/dist/js/adminlte.js"></script>
+<script src="adminSPBT/dist/js/adminlte.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="../adminSPBT/dist/js/pages/dashboard.js"></script>
+<script src="adminSPBT/dist/js/pages/dashboard.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="../adminSPBT/dist/js/demo.js"></script>
+<script src="adminSPBT/dist/js/demo.js"></script>
 
 <script
   src="https://code.jquery.com/jquery-3.4.1.min.js"

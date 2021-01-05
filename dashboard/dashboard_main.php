510f871ad72421d858de111abda1cf0b2f05f388
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2021-01-05 10:13:53 +0800

    Update dashboard_main.php

--- dashboard/dashboard_main.php
+++ dashboard/dashboard_main.php
@@ -163,9 +163,12 @@ $b = 1;
 
 
   <!-- Navbar -->
-  <nav class="navbar navbar-light bg-light">
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light border-bottom">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+      </li>
       <li class="nav-item d-none d-sm-inline-block">
         <a href="../../index.php" class="nav-link">Halaman utama</a>
       </li>

da487d0ec475d2e598f236012b7a01aa10c45d67
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2021-01-08 14:15:06 +0800

    Update main3.php

--- emonitoringNegeri/main3.php
+++ emonitoringNegeri/main3.php
@@ -135,35 +135,16 @@ $a = 1;
   <!-- /.modal -->
   <!-- End salary modal -->
   <!-- Begin salary modal -->
-<div class="modal fade" id="editJudulModal">
-  <div class="modal-dialog">
-    <div class="modal-content bg-light">
-      <div class="modal-header">
-        <h4 class="modal-title">Kemaskini Rekod (Bilangan Stok)</h4>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span></button>
-        </div>
-        <div class="modal-body">
-          <div class="dash4"></div>
-        </div>
-      </div>
-      <!-- /.modal-content -->
-    </div>
-    <!-- /.modal-dialog -->
-  </div>
-  <!-- /.modal -->
-  <!-- End salary modal -->
-  <!-- Begin salary modal -->
-  <div class="modal fade" id="judulModal">
+  <div class="modal fade" id="editJudulModal">
     <div class="modal-dialog">
       <div class="modal-content bg-light">
         <div class="modal-header">
-          <h4 class="modal-title">Kemaskini bilangan Stok</h4>
+          <h4 class="modal-title">Kemaskini Rekod (Bilangan Stok)</h4>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span></button>
           </div>
           <div class="modal-body">
-            <div class="dash3"></div>
+            <div class="dash4"></div>
           </div>
         </div>
         <!-- /.modal-content -->
@@ -172,76 +153,95 @@ $a = 1;
     </div>
     <!-- /.modal -->
     <!-- End salary modal -->
-    <body class="hold-transition sidebar-mini layout-fixed">
-
-      <div class="wrapper">
-
-        <!-- Navbar -->
-        <nav class="main-header navbar navbar-expand navbar-white navbar-light border-bottom">
-          <!-- Left navbar links -->
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
-            </li>
-            <li class="nav-item d-none d-sm-inline-block">
-              <a href="main1.php" class="nav-link">Halaman utama</a>
-            </li>
-            <li class="nav-item d-none d-sm-inline-block">
-              <a href="#" class="nav-link"></a>
-            </li>
-          </ul>
-
-          <!-- SEARCH FORM -->
-          <form class="form-inline ml-3">
-            <div class="input-group input-group-sm">
-              <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-              <div class="input-group-append">
-                <button class="btn btn-navbar" type="submit">
-                  <i class="fas fa-search"></i>
-                </button>
-              </div>
+    <!-- Begin salary modal -->
+    <div class="modal fade" id="judulModal">
+      <div class="modal-dialog">
+        <div class="modal-content bg-light">
+          <div class="modal-header">
+            <h4 class="modal-title">Kemaskini bilangan Stok</h4>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span></button>
             </div>
-          </form>
-
-          <!-- Right navbar links -->
-          <ul class="navbar-nav ml-auto">
-
-            <!-- Exit -->
-            <li class="nav-item dropdown">
-              <a class="nav-link" data-toggle="dropdown" href="../main1.php">
-                <i class="far fa-times-circle"></i>
-              </a>
-              <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                <div class="dropdown-divider"></div>
-                <a href="../logout.php" class="dropdown-item dropdown-footer">Logout</a>
-              </div>
-            </li>
-
-          </ul>
-        </nav>
-        <!-- /.navbar -->
-
-        <!-- Main Sidebar Container -->
-        <aside class="main-sidebar sidebar-light-primary elevation-4">
-          <!-- Brand Logo -->
-          <a href="main1.php" class="brand-link">
-            <img src="../adminSPBT/dist/img/logo_kpm.png" alt="altus Logo" class="brand-image img-circle elevation-3"
-            style="opacity: .8">
-            <span class="brand-text font-weight-dark" style="font-family: 'Fugaz One', cursive;">mySPBT 2.0</span>
-          </a>
-
-          <!-- Sidebar -->
-          <div class="sidebar">
-            <!-- Sidebar user panel (optional) -->
-            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-              <div class="info">
-                <a href="#" class="d-block"><?php echo strtoupper($row_Recordset['nama']);?></a>
-              </div>
+            <div class="modal-body">
+              <div class="dash3"></div>
             </div>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+      </div>
+      <!-- /.modal -->
+      <!-- End salary modal -->
+      <body class="hold-transition sidebar-mini layout-fixed">
 
-            <!-- Sidebar Menu -->
-            <nav class="mt-2">
-              <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <div class="wrapper">
+
+          <!-- Navbar -->
+          <nav class="main-header navbar navbar-expand navbar-white navbar-light border-bottom">
+            <!-- Left navbar links -->
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+              </li>
+              <li class="nav-item d-none d-sm-inline-block">
+                <a href="main1.php" class="nav-link">Halaman utama</a>
+              </li>
+              <li class="nav-item d-none d-sm-inline-block">
+                <a href="#" class="nav-link"></a>
+              </li>
+            </ul>
+
+            <!-- SEARCH FORM -->
+            <form class="form-inline ml-3">
+              <div class="input-group input-group-sm">
+                <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+                <div class="input-group-append">
+                  <button class="btn btn-navbar" type="submit">
+                    <i class="fas fa-search"></i>
+                  </button>
+                </div>
+              </div>
+            </form>
+
+            <!-- Right navbar links -->
+            <ul class="navbar-nav ml-auto">
+
+              <!-- Exit -->
+              <li class="nav-item dropdown">
+                <a class="nav-link" data-toggle="dropdown" href="../main1.php">
+                  <i class="far fa-times-circle"></i>
+                </a>
+                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                  <div class="dropdown-divider"></div>
+                  <a href="../logout.php" class="dropdown-item dropdown-footer">Logout</a>
+                </div>
+              </li>
+
+            </ul>
+          </nav>
+          <!-- /.navbar -->
+
+          <!-- Main Sidebar Container -->
+          <aside class="main-sidebar sidebar-light-primary elevation-4">
+            <!-- Brand Logo -->
+            <a href="main1.php" class="brand-link">
+              <img src="../adminSPBT/dist/img/logo_kpm.png" alt="altus Logo" class="brand-image img-circle elevation-3"
+              style="opacity: .8">
+              <span class="brand-text font-weight-dark" style="font-family: 'Fugaz One', cursive;">mySPBT 2.0</span>
+            </a>
+
+            <!-- Sidebar -->
+            <div class="sidebar">
+              <!-- Sidebar user panel (optional) -->
+              <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                <div class="info">
+                  <a href="#" class="d-block"><?php echo strtoupper($row_Recordset['nama']);?></a>
+                </div>
+              </div>
+
+              <!-- Sidebar Menu -->
+              <nav class="mt-2">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
            with font-awesome or any other icon font library -->
            <li class="nav-item has-treeview menu-open">
@@ -506,7 +506,8 @@ $a = 1;
                       <th>Bil</th>
                       <th>Judul</th>
                       <th>Bil Naskhah (BOSD)</th>
-                      <th>Tindakan</th>
+                      <th>Hapus</th>
+                      <th>Edit</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -516,6 +517,7 @@ $a = 1;
                         <td><?php echo strtoupper($rekodPemantauan2['judul']);?></td>
                         <td><?php echo $rekodPemantauan2['bukuLebihan'];?></td>
                         <td><a data-toggle="modal" data-target="#delJudulModal" data-whatever="<?php echo $rekodPemantauan2['id'];?>" data-whatever2="<?php echo $rekodPemantauan2['kodSekolah'];?>"class="nav-link"><i class="fas fa-times"></i></a></td>
+                        <td><a data-toggle="modal" data-target="#editJudulModal" data-whatever5="<?php echo $rekodPemantauan['id'];?>" data-whatever6="<?php echo $rekodPemantauan['kodSekolah'];?>"class="nav-link"><i class="fas fa-edit"></i></a></td>
                       </tr>
                     <?php } while ($rekodPemantauan2 = mysqli_fetch_assoc($Recordset7)); ?>
                     <tr>

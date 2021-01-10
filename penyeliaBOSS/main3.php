23cc1e795470041ed0ea1f816b2a2facb9ce0def
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2021-01-10 23:20:10 +0800

    Update main3.php

--- penyeliaBOSS/main3.php
+++ penyeliaBOSS/main3.php
@@ -472,7 +472,7 @@ $a = 1;
                         <div class="form-group">
                           Enrolmen keseluruhan murid:
                           <div class="input-group mb-3">
-                            <input type="number" name="enrolmen" class="form-control"  id="validationDefault01" value="<?php echo $dataSekolah['enrolmen'];?>" required>
+                            <input type="text" name="enrolmen" class="form-control"  id="validationDefault01" value="<?php echo $dataSekolah['enrolmen'];?>" required>
                             <input type="hidden" name="remark" class="form-control"  id="validationDefault01" value="observe" >
                             <div class="input-group-append input-group-text">
                               <span class="fas fa-id-card-alt"></span>

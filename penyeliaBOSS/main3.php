93d97a09e3b9c29df76223cdcea3ab16eea38243
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2021-01-10 21:08:12 +0800

    Update main3.php

--- penyeliaBOSS/main3.php
+++ penyeliaBOSS/main3.php
@@ -184,7 +184,7 @@ $a = 1;
                 <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
               </li>
               <li class="nav-item d-none d-sm-inline-block">
-                <a href="main1.php" class="nav-link">Halaman utama</a>
+                <a href="../indexBOSS/index.php" class="nav-link">Halaman utama</a>
               </li>
               <li class="nav-item d-none d-sm-inline-block">
                 <a href="#" class="nav-link"></a>
@@ -208,7 +208,7 @@ $a = 1;
 
               <!-- Exit -->
               <li class="nav-item dropdown">
-                <a class="nav-link" data-toggle="dropdown" href="../main1.php">
+                <a class="nav-link" data-toggle="dropdown" href="../../indexBOSS/index.php">
                   <i class="far fa-times-circle"></i>
                 </a>
                 <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
@@ -224,7 +224,7 @@ $a = 1;
           <!-- Main Sidebar Container -->
           <aside class="main-sidebar sidebar-light-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="main1.php" class="brand-link">
+            <a href="../indexBOSS/index.php" class="brand-link">
               <img src="../adminSPBT/dist/img/logo_kpm.png" alt="altus Logo" class="brand-image img-circle elevation-3"
               style="opacity: .8">
               <span class="brand-text font-weight-dark" style="font-family: 'Fugaz One', cursive;">mySPBT 2.0</span>
@@ -245,7 +245,7 @@ $a = 1;
           <!-- Add icons to the links using the .nav-icon class
            with font-awesome or any other icon font library -->
            <li class="nav-item has-treeview menu-open">
-            <a href="main1.php" class="nav-link active">
+            <a href="../indexBOSS/index.php" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                mySPBT 2.0 Dashboard
@@ -284,7 +284,7 @@ $a = 1;
         </div><!-- /.col -->
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="main1.php">Halaman utama</a></li>
+            <li class="breadcrumb-item"><a href="../indexBOSS/index.php">Halaman utama</a></li>
             <li class="breadcrumb-item active">mySPBT 2.0 | Pengesanan Stok</li>
           </ol>
         </div><!-- /.col -->

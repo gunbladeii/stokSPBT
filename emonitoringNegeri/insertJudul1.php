91f67542db0371475434066944c729cde67d1c37
mohd fisha hafiz bin abidin <fisha.hafiz@moe.gov.my>
2021-01-11 14:21:25 +0800

    Initial commit

---
+++ emonitoringNegeri/insertJudul1.php
@@ -0,0 +1,350 @@
+<?php require('conn.php'); ?>
+<?php session_start();?>
+<?php
+
+date_default_timezone_set("asia/kuala_lumpur");
+$date = date('d-F-Y');
+$datePHP = date('Y-m-d');
+
+$colname_Recordset = "-1";
+if (isset($_SESSION['user'])) {
+  $colname_Recordset = $_SESSION['user'];
+}
+
+$kodPembekal = $_GET['kodPembekal'];
+
+$Recordset = $mysqli->query("SELECT * FROM login WHERE username = '$colname_Recordset'");
+$row_Recordset = mysqli_fetch_assoc($Recordset);
+$totalRows_Recordset = mysqli_num_rows($Recordset);
+
+$Recordset2 = $mysqli->query("SELECT * FROM dataSH WHERE username = '$colname_Recordset'");
+$dataSH = mysqli_fetch_assoc($Recordset2);
+$totalRows_Recordset2 = mysqli_num_rows($Recordset2);
+
+$Recordset3 = $mysqli->query("SELECT dataSH.namaPembekal, dataSH.negeri,login.colorBar,SUM(dataSH.nilaiSH) AS sumnilaiSH FROM dataSH INNER JOIN login ON dataSH.username = login.username WHERE dataSH.kodPembekal = '$kodPembekal'");
+$dataSH2 = mysqli_fetch_assoc($Recordset3);
+$totalRows_Recordset3 = mysqli_num_rows($Recordset3);
+
+
+$a = 1;
+?>
+<!DOCTYPE html>
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>mySPBT 2.0 | Dashboard SH-Negeri</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../adminSPBT/plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bbootstrap 4 -->
+  <link rel="stylesheet" href="../adminSPBT/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="../adminSPBT/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="../adminSPBT/plugins/jqvmap/jqvmap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../adminSPBT/dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="../adminSPBT/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="../adminSPBT/plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="../adminSPBT/plugins/summernote/summernote-bs4.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Anton&family=Fugaz+One&family=Titan+One&display=swap" rel="stylesheet">
+  <!-- chart.js plugin -->
+  <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light border-bottom">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="indexNegeri/index.php" class="nav-link">mySPBT</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="#" class="nav-link"></a>
+      </li>
+    </ul>
+
+    <!-- SEARCH FORM -->
+    <form class="form-inline ml-3">
+      <div class="input-group input-group-sm">
+        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-navbar" type="submit">
+            <i class="fas fa-search"></i>
+          </button>
+        </div>
+      </div>
+    </form>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+
+      <!-- Exit -->
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="epnegeri.php">
+          <i class="far fa-times-circle"></i>
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <div class="dropdown-divider"></div>
+          <a href="../logout.php" class="dropdown-item dropdown-footer">Logout</a>
+        </div>
+      </li>
+     
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-light-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="indexNegeri/index.php" class="brand-link">
+      <img src="../adminSPBT/dist/img/logo_kpm.png" alt="altus Logo" class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-dark" style="font-family: 'Fugaz One', cursive;">mySPBT 2.0</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="info">
+          <a href="#" class="d-block"><?php echo strtoupper($row_Recordset['nama']);?></a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark" style="font-family: 'Fugaz One', cursive;">mySPBT 2.0 Pengesanan Sebut Harga-Negeri</h1>
+            <span class="badge badge-warning"><?php echo strtoupper($row_Recordset['nama']);?></span>
+            <span class="badge badge-info"><?php echo strtoupper($row_Recordset['negeri']);?></span>
+            <p>(*Data berikut dikemaskini sehingga <?php echo $date;?>)</p>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="epnegeri.php">Halaman utama</a></li>
+              <li class="breadcrumb-item active">mySPBT 2.0 | Pengesanan SH-Negeri</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+   
+      <section class="content">
+        <div id="row">
+        <div class="col-md-12">
+           <!-- TABLE: list of publisherSPBT -->
+            <div class="card">
+              <div class="card-header border-transparent">
+                <h3 class="card-title" style="font-family: 'Roboto Condensed', sans-serif;">REKOD PEMANTAUAN PEMBEKALAN</h3>
+                <h2 class="card-title" style="font-size:14px;">(Dikemaskini pada <?php echo $date.' '.$time;?>)</h2>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-widget="remove">
+                    <i class="fas fa-times"></i>
+                  </button>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body p-0">
+                          <div class="container">  
+                            <div class="table-responsive">  
+                          <form method="post" id="update_form">
+                    <div align="center">
+                      <h4 style="font-family: 'Roboto Condensed', sans-serif;">Rekod Judul bagi Pembekal <?php echo strtoupper($dataSH2['namaPembekal']);?></h4>
+                    </div>
+                    <div align="center">
+                        <input type="submit" name="multiple_update" id="multiple_update" class="btn btn-info" value="KEMASKINI" />
+                    </div>
+                    <br />
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-striped">
+                            <thead align="center">
+                                <th width="5%"></th>
+                                <th width="60%">Nama Judul</th>
+                                <th width="5%">Bil. Pesanan</th>
+                                <th width="5%">Bil. Dibekal</th>
+                                <th width="5%">Peratus Pembekalan (%)</th>
+                                <th width="20%">Status Bekal</th>
+                            </thead>
+                            <tbody></tbody>
+                        </table>
+                    </div>
+                </form>
+   </div>  
+  </div>
+                         
+                <!-- /.table-responsive -->
+              </div>
+              </div>
+              </div>
+
+                     
+ </section>
+
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="../adminSPBT/plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="../adminSPBT/plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="../adminSPBT/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="../adminSPBT/plugins/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="../adminSPBT/plugins/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="../adminSPBT/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="../adminSPBT/plugins/jqvmap/maps/jquery.vmap.world.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="../adminSPBT/plugins/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="../adminSPBT/plugins/moment/moment.min.js"></script>
+<script src="../adminSPBT/plugins/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="../adminSPBT/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="../adminSPBT/plugins/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="../adminSPBT/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- FastClick -->
+<script src="../adminSPBT/plugins/fastclick/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="../adminSPBT/dist/js/adminlte.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="../adminSPBT/dist/js/pages/dashboard.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../adminSPBT/dist/js/demo.js"></script>
+
+<!-- DataTables -->
+<script src="jquery.dataTables.js"></script>
+<script src="dataTables.bootstrap4.js"></script>
+<script>
+  $(function () {
+    $("#example1").DataTable();
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+    });
+  });
+</script>
+<script>  
+$(document).ready(function(){  
+    
+    function fetch_data()
+    {
+        $.ajax({
+            url:"selectPantau.php?kodPembekal=<?php echo $kodPembekal;?>&negeri=<?php echo $negeri;?>",
+            method:"POST",
+            dataType:"json",
+            success:function(data)
+            {
+                var html = '';
+                for(var count = 0; count < data.length; count++)
+                {
+                    html += '<tr align="center">';
+                    html += '<td><input type="checkbox" id="'+data[count].id+'" data-kodpembekal="'+data[count].kodpembekal+'" data-judul="'+data[count].judul+'" data-bilnaskhahpesan="'+data[count].bilnaskhahpesan+'" data-bilnaskhahbekal="'+data[count].bilnaskhahbekal+'" data-statusbekal="'+data[count].statusbekal+'" data-peratusbekal="'+data[count].peratusbekal+'" class="check_box"  /></td>';
+                    html += '<td align="left">'+data[count].judul+'</td>';
+                    html += '<td>'+data[count].bilnaskhahpesan+'</td>';
+                    html += '<td>'+data[count].bilnaskhahbekal+'</td>';
+                    html += '<td>'+data[count].peratusbekal+'</td>';
+                    html += '<td style="color:red">'+data[count].statusbekal+'</td></tr>';
+                }
+                $('tbody').html(html);
+            }
+        });
+    }
+
+    fetch_data();
+
+    $(document).on('click', '.check_box', function(){
+        var html = '';
+        if(this.checked)
+        {
+            html = '<td><input type="checkbox" id="'+$(this).attr('id')+'" data-kodpembekal="'+$(this).data('kodpembekal')+'" data-judul="'+$(this).data('judul')+'" data-bilnaskhahpesan="'+$(this).data('bilnaskhahpesan')+'" data-bilnaskhahbekal="'+$(this).data('bilnaskhahbekal')+'" data-statusbekal="'+$(this).data('statusbekal')+'" data-peratusbekal="'+$(this).data('peratusbekal')+'" class="check_box" checked /></td>';
+            html += '<td align="left">'+$(this).data('judul')+'</td>';
+            html += '<td><input type="text" name="bilnaskhahpesan[]" class="form-control" value="'+$(this).data("bilnaskhahpesan")+'" /></td>';
+            html += '<td><input type="text" name="bilnaskhahbekal[]" class="form-control" value="'+$(this).data("bilnaskhahbekal")+'" /></td>';
+            html += '<td><input type="hidden" name="hidden_id[]" value="'+$(this).attr('id')+'" /></td>';
+            html += '<td></td>';
+        }
+        else
+        {
+            html = '<td><input type="checkbox" id="'+$(this).attr('id')+'" data-kodpembekal="'+$(this).data('kodpembekal')+'" data-judul="'+$(this).data('judul')+'" data-bilnaskhahpesan="'+$(this).data('bilnaskhahpesan')+'" data-bilnaskhahbekal="'+$(this).data('bilnaskhahbekal')+'" data-statusbekal="'+$(this).data('statusbekal')+'" data-peratusbekal="'+$(this).data('peratusbekal')+'" class="check_box" /></td>';
+            html += '<td align="left">'+$(this).data('judul')+'</td>';
+            html += '<td>'+$(this).data('bilnaskhahpesan')+'</td>';
+            html += '<td>'+$(this).data('bilnaskhahbekal')+'</td>';
+            html += '<td>'+$(this).data('peratusbekal')+'</td>';
+            html += '<td style="color:red">'+$(this).data('statusbekal')+'</td>';            
+        }
+        $(this).closest('tr').html(html);
+    });
+
+    $('#update_form').on('submit', function(event){
+        event.preventDefault();
+        if($('.check_box:checked').length > 0)
+        {
+            $.ajax({
+                url:"updatePantau.php",
+                method:"POST",
+                data:$(this).serialize(),
+                success:function()
+                {
+                    alert('Data telah dikemaskini');
+                    fetch_data();
+                }
+            })
+        }
+    });
+
+});  
+</script>
+</body>
+</html>
---
+++ penyeliaBOSS/insertJudul3.php
@@ -0,0 +1,36 @@
+<?php
+
+//database_connection.php
+
+$connect = new PDO("mysql:host=localhost;dbname=myspbt", "adminspbt", "Sh@ti5620");
+
+?>
+<?php
+
+//multiple_update.php
+
+
+
+if(isset($_POST['hidden_id']))
+{
+ $bilnaskhahpesan = $_POST['bilnaskhahpesan'];
+ $bilnaskhahbekal = $_POST['bilnaskhahbekal'];
+ $id = $_POST['hidden_id'];
+ for($count = 0; $count < count($id); $count++)
+ {
+  $data = array(
+   ':bilnaskhahpesan'  => $bilnaskhahpesan[$count],
+   ':bilnaskhahbekal' => $bilnaskhahbekal[$count],
+   ':id'   => $id[$count]
+  );
+  $query = "
+  UPDATE dataJudulPenerbit 
+  SET bilNaskhahPesan = :bilnaskhahpesan, bilnaskhahbekal = :bilnaskhahbekal 
+  WHERE id = :id
+  ";
+  $statement = $connect->prepare($query);
+  $statement->execute($data);
+ }
+}
+
+?>

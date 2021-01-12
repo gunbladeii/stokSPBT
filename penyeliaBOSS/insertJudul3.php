955252d4a6f55ba67a6073684f8d8dd73d097391
mohd fisha hafiz bin abidin <fisha.hafiz@moe.gov.my>
2021-01-12 12:07:27 +0800

    Initial commit

--- penyeliaBOSS/insertJudul3.php
+++ penyeliaBOSS/insertJudul3.php
@@ -15,7 +15,6 @@ if(isset($_POST['hidden_id']))
 {
  $bukurosak = $_POST['bukurosak'];
  $bukurosakmurid = $_POST['bukurosakmurid'];
- $bukustok = $_POST['bukustok'];
  $bukulebihan = $_POST['bukulebihan'];
  $id = $_POST['hidden_id'];
  for($count = 0; $count < count($id); $count++)
@@ -23,13 +22,11 @@ if(isset($_POST['hidden_id']))
   $data = array(
    ':bukurosak'  => $bukurosak[$count],
    ':bukurosakmurid' => $bukurosakmurid[$count],
-   ':bukustok' => $bukustok[$count],
    ':bukulebihan' => $bukulebihan[$count],
   );
   $query = "
-  UPDATE dataJudulPenerbit 
-  SET bilNaskhahPesan = :bilnaskhahpesan, bilnaskhahbekal = :bilnaskhahbekal 
-  WHERE id = :id
+  INSERT INTO rekodPemantauan 
+  (bukuRosak, bukuRosakMurid, bukuLebihan)VALUES(bukurosak,bukurosakmurid,bukulebihan)
   ";
   $statement = $connect->prepare($query);
   $statement->execute($data);

324fad33fd47d87bc4ddcdf564f00094931112b7
mohd fisha hafiz bin abidin <fisha.hafiz@moe.gov.my>
2021-01-12 14:29:09 +0800

    Update insertJudul1.php

--- penyeliaBOSS/insertJudul1.php
+++ penyeliaBOSS/insertJudul1.php
@@ -296,7 +296,7 @@ $(document).ready(function(){
                     html += '<td align="left">'+data[count].harga+'</td>';
                     html += '<td align="left">'+data[count].darjahtingkatan+'</td>';
                     html += '<td></td>';
-                    html += '<td></td><th>';
+                    html += '<td></td><tr>';
                 }
                 $('tbody').html(html);
             }

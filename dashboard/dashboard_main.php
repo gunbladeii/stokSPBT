3624102352148c249fc1dc8ddbc11a710a4be5c2
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2021-02-19 21:07:31 +0800

    Update dashboard_main.php

--- dashboard/dashboard_main.php
+++ dashboard/dashboard_main.php
@@ -181,7 +181,7 @@ $b = 1;
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <img src="../emonitoring/img/logo MySPBTdashboard.png" style="width:20%;height:60%">
+            <img src="../emonitoring/img/logo MySPBTdashboard.png" style="width:15%;height:60%">
           </div><!-- /.col -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->

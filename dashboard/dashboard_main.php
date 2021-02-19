c76472d4a27605f21cd448b2416f00742159f1c6
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2021-02-19 21:00:06 +0800

    Update dashboard_main.php

--- dashboard/dashboard_main.php
+++ dashboard/dashboard_main.php
@@ -181,7 +181,7 @@ $b = 1;
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark" style="font-family: 'Fugaz One', cursive;">mySPBT 2.0 System Dashboard</h1>
+            <img src="../emonitoring/img/logoMySPBTdashboard.png" style="width:60%;opacity: .8">
           </div><!-- /.col -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
